31cffbd80b0eede617b177a48b07e0cda6f44cb8
RK Bernardino Macion <memoclarkson03@gmail.com>
2022-10-20 19:41:04 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import {
   Input, SkeletonText,
   Text
 } from '@chakra-ui/react'
-import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api'
-import { useState } from 'react'
+import { GoogleMap, Marker, useJsApiLoader, Autocomplete, DirectionsRenderer } from '@react-google-maps/api'
+import { useRef, useState } from 'react'
 import { FaLocationArrow, FaTimes } from 'react-icons/fa'
 
 const center = { lat: 10.3157, lng: 123.8854 }
@@ -18,12 +18,47 @@ function App() {
 
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries: ['places'],
   })
 
   const [ map, setmap ] = useState(/** @type google.maps.Map */(null))
+  const [directionResponse, setDirectionResponse] = useState(null)
+  const [distance, setDistance] = useState('')
+  const [duration, setDuration] = useState('')
+
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const originRef = useRef()
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const destinationRef = useRef()
 
   if (!isLoaded) {
     return <SkeletonText />
+  }
+
+  async function calculateRoute() {
+    if (originRef.current.value === '' || destinationRef.current.value === '') {
+      return
+    }
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService()
+    const results = await directionsService.route({
+      origin: originRef.current.value,
+      destination: destinationRef.current.value,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING
+    })
+    setDirectionResponse(results)
+    setDistance(results.routes[0].legs[0].distance.text)
+    setDuration(results.routes[0].legs[0].duration.text)
+
+  }
+
+  function clearRoute() {
+    setDirectionResponse(null)
+    setDistance('')
+    setDuration('')
+    originRef.current.value = ''
+    destinationRef.current.value = '' 
   }
 
   return (
@@ -50,6 +85,7 @@ function App() {
 
         >
           <Marker position={center} />
+          {directionResponse && <DirectionsRenderer directions={directionResponse}/> }
         </GoogleMap>
       </Box>
 
@@ -63,22 +99,29 @@ function App() {
         zIndex='modal'
       >
         <HStack spacing={4}>
-          <Input type='text' placeholder='Origin' />
-          <Input type='text' placeholder='Destination' />
+
+          <Autocomplete>
+          <Input type='text' placeholder='Origin' ref={originRef}/>
+          </Autocomplete>
+
+          <Autocomplete>
+          <Input type='text' placeholder='Destination' ref={destinationRef}/>
+          </Autocomplete>
+
           <ButtonGroup>
-            <Button colorScheme='pink' type='submit'>
+            <Button colorScheme='pink' type='submit' onClick={calculateRoute}>
               Calculate Route
             </Button>
             <IconButton
               aria-label='center back'
               icon={<FaTimes />}
-              onClick={() => alert(123)}
+              onClick={clearRoute}
             />
           </ButtonGroup>
         </HStack>
         <HStack spacing={4} mt={4} justifyContent='space-between'>
-          <Text>Distance: </Text>
-          <Text>Duration: </Text>
+          <Text>Distance: {distance}</Text>
+          <Text>Duration: {duration}</Text>
           <IconButton
             aria-label='center back'
             icon={<FaLocationArrow />}
